54386d81a153fc1b4161f4555804394503163bdf
Moysenko <moysenkom@gmail.com>
2020-05-11 14:51:02 +0500

    load from save added

--- farmer.py
+++ farmer.py
@@ -6,43 +6,47 @@ from selenium.webdriver.support.wait import WebDriverWait
 import os
 import time
 import random
+import glob
 
-cur_dir = os.path.dirname(os.path.abspath(__file__)) + '/saves'
+cur_dir = os.path.dirname(os.path.abspath(__file__))
 driver = None
 
 
 def _open_site():
     global driver
     chrome_options = webdriver.ChromeOptions()
-    preference = {'download.default_directory': cur_dir,
+    preference = {'download.default_directory': os.path.join(cur_dir, 'output_saves'),
                   "safebrowsing.enabled": "false"}
     chrome_options.add_experimental_option('prefs', preference)
+    chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 
     driver.get('https://orteil.dashnet.org/cookieclicker/')
 
 
-def click_ups():
-    upgrades = []
-    ind = 0
-    while True:
-        try:
-            upgrades.append(driver.find_element_by_id(f'upgrade{ind}'))
-            ind += 1
-        except:
-            break
+def print_stats():
+    print(driver.find_element_by_id("cookies").text)
 
-    products = []
+
+def list_numerated_elements_by_id(name):
+    elements = []
     ind = 0
     while True:
         try:
-            products.append(driver.find_element_by_id(f'product{ind}'))
+            elements.append(driver.find_element_by_id('{0}{1}'.format(name, ind)))
             ind += 1
         except:
             break
+    return elements
+
+
+def click_ups():
+    upgrades = list_numerated_elements_by_id('upgrade')
+    products = list_numerated_elements_by_id('product')
 
     last_bad_product = len(products)
     last_bad_upgrade = len(upgrades)
+
     for i in range(5):
         if random.random() < 1/2:
             for ind in range(last_bad_product, -1, -1):
@@ -60,6 +64,11 @@ def click_ups():
                     continue
 
 
+def refresh_info():
+    click_ups()
+    print_stats()
+
+
 def _farm(farm_time):
     while True:
         try:
@@ -71,38 +80,68 @@ def _farm(farm_time):
             pass
 
     time_end = time.time() + farm_time
-    next_time_update = 100
+    next_refresh = 100
     while time.time() < time_end:
         bigCookie.click()
-        next_time_update -= 1
-        if next_time_update <= 0:
-            click_ups()
-            next_time_update = random.choice([i * 100 for i in range(1, 5)])
+        next_refresh -= 1
+        if next_refresh <= 0:
+            refresh_info()
+            next_refresh = random.choice([i * 100 for i in range(1, 5)])
 
 
-def _save():
+def _open_options():
     while True:
         try:
             options = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.XPATH, '//*[@id="prefsButton"]')))
+                EC.element_to_be_clickable((By.ID, 'prefsButton')))
             options.click()
             break
         except:
             pass
 
-    save_to_file = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="menu"]/div[3]/div[4]/a[1]')))
+
+def _save():
+    output_saves_dir = os.path.join(cur_dir, 'output_saves')
+    for f in glob.glob(os.path.join(output_saves_dir, '*')):
+        os.remove(f)
+
+    _open_options()
+
+    save_to_file = WebDriverWait(driver, 40).until(
+        EC.presence_of_element_located((By.LINK_TEXT, 'Save to file')))
     save_to_file.click()
 
-    while len(os.listdir(cur_dir)) == 0:
+    while not os.listdir(output_saves_dir):
         time.sleep(1)
 
     driver.quit()
 
 
+def _get_farm_time():
+    return int(input('Time in seconds to farm: '))
+
+
+def _get_save_name():
+    return input('Name of file in input_saves with save: ')
+
+
+def _open_save():
+    save_name = _get_save_name()
+    if not save_name:
+        return
+
+    _open_options()
+
+    save_to_file_button = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, 'FileLoadInput')))
+    save_file = os.path.join(cur_dir, 'input_saves',  save_name)
+    save_to_file_button.send_keys(save_file)
+
+
 def main():
     _open_site()
-    farm_time = 600
+    _open_save()
+    farm_time = _get_farm_time()
     _farm(farm_time)
     _save()
 
