0e047957b4e8f85124bed9d3b22ccf180ee18c47
moysenko <moysenkom@gmail.com>
2020-05-12 04:56:39 +0500

    tests added

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 import browser
 import save_manager
 import timeout_decorator
+import clicker
 
 
 class TestBrowser(unittest.TestCase):
@@ -12,12 +13,12 @@ class TestBrowser(unittest.TestCase):
         try:
             site.open()
         except Exception:
-            self.fail("site.open() raised ExceptionType unexpectedly!")
+            self.fail("site.open() raised Exception unexpectedly!")
 
         try:
             site.close()
         except Exception:
-            self.fail("site.close() raised ExceptionType unexpectedly!")
+            self.fail("site.close() raised Exception unexpectedly!")
 
 
 class TestSaveManager(unittest.TestCase):
@@ -29,4 +30,58 @@ class TestSaveManager(unittest.TestCase):
         try:
             save_manager._open_options(site.driver)
         except Exception:
-            self.fail("_open_options() failed!")
+            self.fail("_open_options() raised Exception unexpectedly!")
+        site.close()
+
+    @timeout_decorator.timeout(50)
+    def test_load_from_save_file(self):
+        site = browser.Site()
+        site.open()
+        try:
+            save_manager.load_from_save_file(site.driver, 'test.txt')
+        except Exception:
+            self.fail("load_from_save_file() raised Exception unexpectedly!")
+        site.close()
+
+    @timeout_decorator.timeout(50)
+    def test_download_save_file(self):
+        site = browser.Site()
+        site.open()
+        try:
+            save_manager.download_save_file(site.driver)
+        except Exception:
+            self.fail("download_save_file() raised Exception unexpectedly!")
+        site.close()
+
+
+class TestClicker(unittest.TestCase):
+
+    @timeout_decorator.timeout(120)
+    def test_click_ups(self):
+        site = browser.Site()
+        site.open()
+        try:
+            clicker.click_ups(site.driver)
+        except Exception:
+            self.fail("click_ups() raised Exception unexpectedly!")
+        site.close()
+
+    @timeout_decorator.timeout(120)
+    def test_print_stats(self):
+        site = browser.Site()
+        site.open()
+        try:
+            clicker.print_stats(site.driver)
+        except Exception:
+            self.fail("print_stats() raised Exception unexpectedly!")
+        site.close()
+
+    @timeout_decorator.timeout(120)
+    def test_farm(self):
+        site = browser.Site()
+        site.open()
+        try:
+            clicker.farm(site.driver, 30)
+        except Exception:
+            self.fail("farm() raised Exception unexpectedly!")
+        site.close()
